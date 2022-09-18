11df1890dc65cf5379d168da0b137f47f205d4cc
zhuohengfeng <zhuo_hf@foxmail.com>
2022-09-18 22:41:18 +0800

    [feat] add opengl lib

--- main.cpp
+++ main.cpp
@@ -16,13 +16,22 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    SDL_Window *window = SDL_CreateWindow("Demo",
+    SDL_Window *window = SDL_CreateWindow("3D Game",
                                           SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
                                           800, 600, SDL_WINDOW_ALLOW_HIGHDPI);
     if (window == nullptr) {
         cout << "SDL_CreateWindow Error: " << SDL_GetError() << endl;
         return 1;
     }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+    SDL_Surface* image = SDL_LoadBMP("../resource/123.bmp");
+    if (image == nullptr) {
+        cerr << "SDL load bmp failed"<<endl;
+        return -1;
+    }
+    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, image);
+
 
     SDL_Event event;
     while (true) {
@@ -32,8 +41,14 @@ int main(int argc, char *argv[])
                 break;
             }
         }
+        SDL_Rect rect = {0, 0, 800, 600};
+        SDL_RenderCopy(renderer, texture, nullptr, &rect);
+        SDL_RenderPresent(renderer);
     }
 
+    SDL_DestroyTexture(texture);
+    SDL_FreeSurface(image);
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
 
